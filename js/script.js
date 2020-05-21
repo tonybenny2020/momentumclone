06532a11be72bae082b3066b6a6c1bfbd456620c
tonybenny <tonybenny2004@gmail.com>
2020-05-21 14:48:52 +0100

    "Adding storage"

--- js/script.js
+++ js/script.js
@@ -1,4 +1,18 @@
 document.addEventListener('DOMContentLoaded',function(){
 const images = ['img1.jpg',"img2.jpg","img3.jpg"];
 const image = images[Math.floor(Math.random()*images.length)] ;
-document.getElementById("image").setAttribute("src",`./images/${image}`)},false);
+document.getElementById("image").setAttribute("src",`./images/${image}`);
+var focusInput = document.getElementById("focus");
+
+chrome.storage.sync.get(["focus"],function(result){
+ 	if(result.focus)
+ 	{
+
+ 		focusInput.value = result.focus;
+ 	}
+ });
+focusInput.addEventListener('keypress',function (e) {
+ chrome.storage.sync.set({focus: e.target.value});
+});
+
+},false);
